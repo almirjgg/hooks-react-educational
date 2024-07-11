e2cfcc33b42e4bbcb49a4e97c097a8268f65b708
almirjgg <almirjgg@gmail.com>
2024-07-10 23:48:31 -0300

    cleaning names

--- src/09-useContext/context/UserProvider.jsx
+++ src/09-useContext/context/UserProvider.jsx
@@ -1,22 +1,19 @@
-import { useState } from "react"
-import { UserContext } from "./UserContext"
+import { useState } from 'react';
+import { UserContext } from './UserContext';
 
 // const user = {
 //     id: 123,
-//     name: 'Fernando Herrera',
-//     email: 'fernando@google.com'
+//     name: 'almir',
+//     email: 'a@google.com'
 // }
 
-
-
 export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState();
 
-    const [user, setUser] = useState();
-
-    return (
-        // <UserContext.Provider value={{ hola: 'Mundo', user: user }}>
-        <UserContext.Provider value={{ user, setUser }}>
-            { children }
-        </UserContext.Provider>
-    )
-}
+  return (
+    // <UserContext.Provider value={{ hola: 'Mundo', user: user }}>
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
